851857b835dd5e6a227e000cb8bf4c228bdf5af3
johnmcl81 <j.d.mclachlan@gmail.com>
2015-01-26 20:43:25 +0100

    Gallery admin display tweak

--- apps/portfolio-cms/app/views/admin/gallery/index.blade.php
+++ apps/portfolio-cms/app/views/admin/gallery/index.blade.php
@@ -7,13 +7,12 @@
         @foreach($galleryitems as $galleryitem)
             <tr>
                 <td width = 20%><a href="{{ action('ProjectsController@show', $galleryitem->id) }}">{{ HTML::image($galleryitem->image_url, $galleryitem->image_link, array('class' => 'thumb'))}}</a></td>
-                <td width = 40%>{{link_to_action('ProjectsController@show',$galleryitem->title, $galleryitem->id, array('target'=>'_blank'))}}</td>
+                <td width = 30%>{{link_to_action('ProjectsController@show',$galleryitem->title, $galleryitem->id, array('target'=>'_blank'))}}</td>
                     @foreach ($categories as $category)
                     @if ($galleryitem->category_id == $category->id)
                         <td width = 30%>{{ $category->category }} </td>
                     @endif
                 @endforeach
-                <td width = 20%>{{ $galleryitem->image_link }} </td>
                 <td>{{link_to_action('GalleryController@edit','Edit',$galleryitem->id,['class' => 'tiny button'])}}</td>
                 {{ Form::model($galleryitem, array('action' => ['GalleryController@destroy', $galleryitem->id], 'method' => 'delete')) }}
                 <td>{{ Form::button('Delete', ['type' => 'submit', 'class' => 'tiny alert button']) }}</td> 
